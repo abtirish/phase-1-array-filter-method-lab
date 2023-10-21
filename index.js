5b2c07f78d91d9a271c134de2bd1efa12cdb2d7e
abtirish <abtirish@gmail.com>
2023-10-20 20:32:59 -0600

    phase-1-array-filter-method-lab

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution here
+
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+//Use array.prototype.filter to find a matching name to the drivers array
+function findMatching(drivers, name) {
+  return drivers.filter(function(driver) {
+    return driver.toLowerCase() === name.toLowerCase();
+  });
+}
+
+console.log(findMatching(drivers, 'bobby'));
+console.log(findMatching(drivers, 'Sammy'))
+console.log(findMatching(drivers, 'Susan'))
+
+function fuzzyMatch(drivers, name) {
+    return drivers.filter(function(driver){
+        return driver.toLowerCase().startsWith(name.toLowerCase())
+    });
+}
+
+console.log(fuzzyMatch(drivers, 'Sa'));
+
+function matchName(drivers, name) {
+    return drivers.filter(function(driverName){
+        return driverName.name === name;
+    })
+}
+console.log(matchName(drivers, 'Bobby'))
